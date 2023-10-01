e4d1d5efcc012671086deb19725080316121d98e
mruizc20222 <144749960+mruizc20222@users.noreply.github.com>
2023-10-01 14:26:25 -0500

    Create foreach.js

---
+++ foreach.js
@@ -0,0 +1,13 @@
+var galaxies = [3,4,67,67,3,12,6,7]
+function contarEstrellas(galaxies) {
+   var tota = 0;
+   galaxies.forEach( 
+     function (stars){
+      total = stars;
+}
+);
+return total;
+}
+
+var result = contarEstrellas(galaxies);
+document.querySelector ('#header').innerHTML = result
