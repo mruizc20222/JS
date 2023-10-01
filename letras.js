bc1ae346e050d426e2da0f016f310b055def4c08
mruizc20222 <144749960+mruizc20222@users.noreply.github.com>
2023-10-01 15:35:13 -0500

    Create letras.js

---
+++ letras.js
@@ -0,0 +1,14 @@
+ const letters=['a','b','c','d','e'];
+
+ const f=function(){
+  console.log("un elemento.");
+ }
+ letters.forEach(f);
+
+
+
+ letters.forEach(function() {
+    console.log("un elemento");
+ })
+
+ letters.forEach(()=>console.log("un elemento"));
