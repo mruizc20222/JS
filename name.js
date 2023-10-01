2e2e44690b075bc49a1e13de07e451e04d0031c8
mruizc20222 <144749960+mruizc20222@users.noreply.github.com>
2023-10-01 14:38:37 -0500

    Create name.js

---
+++ name.js
@@ -0,0 +1,7 @@
+const nombres = ["Leidy","Alejandro","Esteban","Carolina"];
+function imprimirNombres() {
+    for (let i = 0; i < nombres.length; i++) {
+        document.write(nombres[i] + "<br>");  
+    }
+}
+imprimirNombres();
