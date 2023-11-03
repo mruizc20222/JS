343bc86153a29b9269c8a280c02561cfe8030d38
mruizc20222 <144749960+mruizc20222@users.noreply.github.com>
2023-11-02 19:23:22 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,11 +1,11 @@
 const data = [
   {
     "id": 1,
-    "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    "title": "Collar para perros graduable, resistnte al agua",
     "price": 109.95,
-    "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-    "category": "men's clothing",
-    "image": "collar perros.png",
+    "description": "Tiras reflectantes en la parte delantera y trasera para mayor seguridad y visibilidad. asa de trfico suave al tacto para mayor seguridad cuando sea necesario",
+    "category": "caninos",
+    "image": "collar.jpg",
     "rating": {
       "rate": 3.9,
       "count": 120
@@ -13,7 +13,7 @@ const data = [
   },
   {
     "id": 2,
-    "title": "Mens Casual Premium Slim Fit T-Shirts ",
+    "title": "Casa para perros de razas mdianas ",
     "price": 22.3,
     "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
     "category": "men's clothing",
@@ -25,7 +25,7 @@ const data = [
   },
   {
     "id": 3,
-    "title": "Mens Cotton Jacket",
+    "title": "Cama para perros medianos",
     "price": 55.99,
     "description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
     "category": "men's clothing",
@@ -37,7 +37,7 @@ const data = [
   },
   {
     "id": 4,
-    "title": "Mens Casual Slim Fit",
+    "title": "Cama para perros medianos",
     "price": 15.99,
     "description": "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
     "category": "men's clothing",
