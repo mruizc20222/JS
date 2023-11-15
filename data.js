987c519c630387e026afecaa31fb6acd35c1ebd1
mruizc20222 <144749960+mruizc20222@users.noreply.github.com>
2023-11-14 19:42:38 -0500

    Update data.js

--- data.js
+++ data.js
@@ -35,18 +35,7 @@ const data = [
       "count": 500
     }
   },
-  {
-    "id": 4,
-    "title": "Cama para perros medianos",
-    "price": 15.99,
-    "description": "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
-    "category": "men's clothing",
-    "image": "cama2.jpg",
-    "rating": {
-      "rate": 2.1,
-      "count": 430
-    }
-  },
+
 
 ]
 
