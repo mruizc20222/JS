ba3283c488ff35d1a11c40972336a8ba084b31ea
mruizc20222 <144749960+mruizc20222@users.noreply.github.com>
2023-11-22 19:19:58 -0500

    Update data.js

--- data.js
+++ data.js
@@ -37,18 +37,18 @@ const data = [
   },
   {
     "id": 4,
-    "title": "Cama para perros medianos",
-    "price": 55.99,
-    "description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
-    "category": "men's clothing",
-    "image": "cama.jpg",
+    "title": "Pelotas",
+    "price": 56,
+    "description": "Juego de 3 pelotas para perros de todas las razas",
+    "category": "caninos",
+    "image": "pelotas.jpg",
     "rating": {
       "rate": 4.7,
       "count": 500
     }
   },
   {
-    "id": 3,
+    "id": 5,
     "title": "Cama para perros medianos",
     "price": 55.99,
     "description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
